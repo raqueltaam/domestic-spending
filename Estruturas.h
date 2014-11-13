50ce84ad18b75627b26d11829990cf3ba0f44a50
carvalhaog <gabriel.infosantos@gmail.com>
2014-11-13 15:23:37 -0200

    Create Estruturas.h
    biblioteca com os Structs do sistema

---
+++ Estruturas.h
@@ -0,0 +1,34 @@
+#ifndef ESTRUTURAS
+#define ESTRUTURAS
+
+typedef struct {
+    int dia, mes, ano;
+} tp_data;
+
+typedef struct
+{
+    int id;
+    char nome[50];
+    float valor;
+    tp_data lancamento;
+} ContaExtraordinaria;
+
+typedef struct
+{
+    int id;
+    char nome[50];
+    float valor;
+    int parcelas;
+    tp_data lancamento;
+    int diaVencimento;
+} ContaRecorrente;
+
+typedef struct
+{
+    int id;
+    char nome[50];
+    float valor;
+    tp_data vencimento;
+} ContaPeriodica;
+
+#endif // ESTRUTURAS
