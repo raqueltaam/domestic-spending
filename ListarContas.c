bc08544f44b96cf59809590b8ce7463394546088
Vinicus Viana <vini_tkv@hotmail.com>
2014-11-13 11:03:05 -0200

    Update ListarContas.c

--- ListarContas.c
+++ ListarContas.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#define ERRO_ABERTURA NULL
 
+const int fim = 0;
 enum contas {contasExtraordinarias = 1, contasRecorrentes, contasPeriodicas};
 //estou mostrando todas as contas extraordinarias
 void mostrarContasExtraordinarias (FILE *arq)
@@ -7,7 +9,7 @@ void mostrarContasExtraordinarias (FILE *arq)
     ContaExtraordinaria conta;
     rewind(arq); //colocando o ponteiro do arquivo no inicio do arquivo
 
-    while (fread(&conta, sizeof(ContaExtraordinaria), 1, arq) != 0) //lendo aruivo e printando na tela 
+    while (fread(&conta, sizeof(ContaExtraordinaria), 1, arq) != fim) //lendo aruivo e printando na tela 
     {
         printf("\nConta Extraordinaria");
         printf("\nID: %d", conta.id);
@@ -22,7 +24,7 @@ void mostrarContasRecorrentes (FILE *arq)
     ContaRecorrente conta;
     rewind(arq);
 
-    while (fread(&conta, sizeof(ContaRecorrente), 1, arq) != 0)
+    while (fread(&conta, sizeof(ContaRecorrente), 1, arq) != fim)
     {
         printf("\nConta Recorrente");
         printf("\nID: %d", conta.id);
@@ -39,7 +41,7 @@ void listarContasPeriodicas(FILE *arq)
     ContaPeriodica conta;
     rewind(arq);
 
-    while (fread(&conta, sizeof(ContaPeriodica), 1, arq) != 0)
+    while (fread(&conta, sizeof(ContaPeriodica), 1, arq) != fim)
     {
         printf("\nConta Periodica");
         printf("\nID: %d", conta.id);
@@ -55,7 +57,7 @@ void listarContasExtraordinarias()
     char nomearq[] = "ContasExtraordinarias.dat";
 
     arquivo = fopen(nomearq, "rb"); //abrindo o arquivo
-    if (arquivo != NULL) //verificando se ele abriu 
+    if (arquivo != ERRO_ABERTURA) //verificando se ele abriu 
     {
         mostrarContasExtraordinarias(arquivo); //mostro as contas
     fclose(arquivo); //fecho o arquivo
@@ -70,7 +72,7 @@ void listarContasRecorrentes()
     char nomearq[] = "ContasRecorrentes.dat";
 
     arquivo = fopen(nomearq, "rb");
-    if (arquivo != NULL)
+    if (arquivo != ERRO_ABERTURA)
     {
         mostrarContasRecorrentes(arquivo);
     fclose(arquivo);
@@ -85,7 +87,7 @@ void listarContasPeriodicas()
     char nomearq[] = "ContasPeriodicas.dat";
 
     arquivo = fopen(nomearq, "rb");
-    if (arquivo != NULL)
+    if (arquivo != ERRO_ABERTURA)
     {
         mostrarContasPeriodicas(arquivo);
     fclose(arquivo);
@@ -119,12 +121,12 @@ void listarContas()
         printf("\t0 - Retornar ao menu aterior\n");
         printf("\nDigite a opcao: ");
         scanf("%d", &menu);
-        if (menu != 0)
+        if (menu != fim)
             if ((menu >= contasExtraordinarias) && (menu <= contasPeriodicas)) //verificando se o usuario digito a opção valida
                 selecionarTipoConta(menu);
             else
                 printf("\n\nOpcao Invalida!\n\n");
-    } while (menu != 0)
+    } while (menu != fim)
     mostrarMenu(); //voltando para o menu inicial 
 }
 
